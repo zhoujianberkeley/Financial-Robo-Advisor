016a9ce3c6b8ed078ee5785f9fb1db9aa8106214
Jian <zhoujian@berkeley.edu>
2019-03-23 18:20:44 +0800

    useless

--- stock_selection.py
+++ stock_selection.py
@@ -33,43 +33,40 @@ x_temp = []
 Qua_Dict = {1:'-04-27', 2:'-07-17', 3:'-10-17',4:'-04-17'}
 
 
-for year in year_list:
-	for quarter in quarter_list:
-		print (year, quarter)
-		df = ts.get_profit_data(year, quarter)
-		print ( df.shape )
-		print ( df )
-		df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True )
-		print ( df )
-		df.sort_values( by="code" , ascending=True, inplace=True )
-		print ( df )
-		df.drop_duplicates( ['code'], inplace = True )
-		print ( df )
-		df['yield'] = np.nan
-		print ( df )
-		( temp_row, temp_col ) = df.shape
-		print (temp_row, temp_col)
-		temp_row = test_less_row ##################################################################
-		for row in range(0, temp_row):
-			print (row)
-			temp_stock = df.iloc[ row, 0 ]
-			temp_df = ts.get_hist_data( temp_stock, start = start_list[quarter], end = end_list[quarter] )
-			#print (temp_df)
-			temp_row2 = -1
-			temp_col2 = -1
-			( temp_row2, temp_col2 ) = temp_df.shape
-			if temp_row2 > 100:
-				yield_yoy = ( temp_df.iloc[ 0, 2 ] - temp_df.iloc[ -1, 2 ] ) / temp_df.iloc[ -1, 2 ]
-				df.iloc[ row, -1 ] = yield_yoy
+df = pd.read_csv('stock_data.csv')#ts.get_profit_data(year, quarter)
+print ( df.shape )
+print ( df )
+df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True )
+print ( df )
+df.sort_values( by="code" , ascending=True, inplace=True )
+print ( df )
+df.drop_duplicates( ['code'], inplace = True )
+print ( df )
+df['yield'] = np.nan
+print ( df )
+( temp_row, temp_col ) = df.shape
+print (temp_row, temp_col)
+temp_row = test_less_row ##################################################################
+for row in range(0, temp_row):
+	print (row)
+	temp_stock = df.iloc[ row, 0 ]
+	temp_df = ts.get_hist_data( temp_stock, start = start_list[quarter], end = end_list[quarter] )
+	#print (temp_df)
+	temp_row2 = -1
+	temp_col2 = -1
+	( temp_row2, temp_col2 ) = temp_df.shape
+	if temp_row2 > 100:
+		yield_yoy = ( temp_df.iloc[ 0, 2 ] - temp_df.iloc[ -1, 2 ] ) / temp_df.iloc[ -1, 2 ]
+		df.iloc[ row, -1 ] = yield_yoy
 
-		df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True )
-		( temp_row, temp_col ) = df.shape
-		for i in range(0, temp_row):
-			for j in range(2, temp_col-1):
-				x_temp.append(df.iloc[i, j])
-			x.append( x_temp )
-			y.append( df.iloc[i, -1] )
-			x_temp = []
+df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True )
+( temp_row, temp_col ) = df.shape
+for i in range(0, temp_row):
+	for j in range(2, temp_col-1):
+		x_temp.append(df.iloc[i, j])
+	x.append( x_temp )
+	y.append( df.iloc[i, -1] )
+	x_temp = []
 
 print ('x=')
 print (x)
@@ -137,7 +134,7 @@ def choosing_stock(the_year, the_quarter):
 
 ##################################################################
 
-choosing_stock(2018, 1)
+choosing_stock(2018, 3)
 
 
 
