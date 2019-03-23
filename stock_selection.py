5c7595d53f80b130b77dc60d5f4f36f05ed25fee
liudeming <liudeming@liudemingdeMacBook-Pro.local>
2019-03-23 18:08:42 +0800

    add_grid_cv

--- stock_selection.py
+++ stock_selection.py
@@ -12,11 +12,15 @@ from sklearn.datasets import make_classification
 
 Model_Rf = RandomForestClassifier()
 
+test_less_row = 100
+
 
 pd.set_option('display.max_columns', None)
 
-year_list = range(2000, 2018)
-quarter_list = range(4)
+year_list = range(2017, 2018)
+quarter_list = range(1, 2)
+start_list = ['2017-04-17', '2017-07-17', '2017-10-17', '2018-01-31']
+end_list = ['2018-04-17', '2018-07-17', '2018-10-17', '2019-01-31']
 
 temp_row = -1
 temp_col = -1
@@ -45,7 +49,7 @@ for year in year_list:
 		print ( df )
 		( temp_row, temp_col ) = df.shape
 		print (temp_row, temp_col)
-		#temp_row = 10
+		temp_row = test_less_row ##################################################################
 		for row in range(0, temp_row):
 			print (row)
 			temp_stock = df.iloc[ row, 0 ]
@@ -81,13 +85,16 @@ print (y_train)
 print ('y_test =')
 print (y_test)
 
+
+
 cv = StratifiedKFold(n_splits= 5, shuffle= True)
-C = range(0, 5, 0.5)
+C = np.arange(0.5, 5, 0.5)
 param_grid = dict(C = C)
 kfold = StratifiedKFold(n_splits = 5, shuffle = True, random_state = 7)
 
 model = SVR()
 grid_search = GridSearchCV(model, param_grid, scoring = 'neg_mean_squared_error', n_jobs = -1, cv = kfold)
+y_train = np.zeros(len(y_train))
 grid_result = grid_search.fit(x_train, y_train)
 print("Best: %f using %s" % (grid_result.best_score_,grid_search.best_params_))
 
@@ -111,7 +118,7 @@ def choosing_stock(the_year, the_quarter):
 	data_choosing_stock.drop_duplicates( ['code'], inplace = True )
 	data_choosing_stock['yield'] = np.nan
 	( row_choosing_stock, col_choosing_stock ) = data_choosing_stock.shape
-	#row_choosing_stock = 10
+	row_choosing_stock = test_less_row ######################################################
 	for the_row in range(0, row_choosing_stock):
 		for the_col in range(2, col_choosing_stock-1):
 			x_temp_choosing_stock.append(data_choosing_stock.iloc[the_row, the_col])
