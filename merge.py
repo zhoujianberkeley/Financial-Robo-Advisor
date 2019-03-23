58cac045182bd6eff382044f462da3f4af63bdf9
liudeming <liudeming@liudemingdeMacBook-Pro.local>
2019-03-23 21:44:29 +0800

    string&int problem

--- merge.py
+++ merge.py
@@ -2,18 +2,36 @@ import pandas as pd
 import tushare as ts 
 from adjust_start_date import Closest_TraDt
 
+row = -1
+col = -1
+
 def get_temp_data(year, quarter):
 	df1 = ts.get_report_data(year, quarter)
+	print (1)
+	print (df1)
 	df1 = df1.merge(ts.get_profit_data(year, quarter), how = 'inner', on = ['code', 'name'])
+	print (2)
+	print (df1)
 	df1 = df1.merge(ts.get_operation_data(year, quarter), how = 'inner', on = ['code', 'name'])
+	print (3)
+	print (df1)
 	df1 = df1.merge(ts.get_growth_data(year, quarter), how = 'inner', on = ['code', 'name'])
+	print (4)
+	print (df1)
 	df1 = df1.merge(ts.get_debtpaying_data(year, quarter), how = 'inner', on = ['code', 'name'])
+	print (5)
+	print (df1)
 	df1 = df1.merge(ts.get_cashflow_data(year, quarter), how = 'inner', on = ['code', 'name'])
+	print (6)
+	print (df1)
+	( row, col ) = df1.shape
+	for i in range(0, row):
+		df1.iloc[i, 0] = str( df1.iloc[i, 0] )
 	return df1
 
 count = 0
-for i in range(2016, 2019):
-	for j in range(1, 5):
+for i in range(2016, 2017):
+	for j in range(1, 2):
 		print (i)
 		print (j)
 		if count == 0:
