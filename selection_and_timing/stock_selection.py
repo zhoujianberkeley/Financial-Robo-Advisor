f3981b2fed5e4cb5a6240fa7a675d801437c057f
Jian <zhoujian@berkeley.edu>
2019-04-08 21:23:46 +0800

    Jian

--- selection_and_timing/stock_selection.py
+++ selection_and_timing/stock_selection.py
@@ -14,7 +14,7 @@ from selection_and_timing.data_merge import get_temp_data
 
 Model_Rf = RandomForestClassifier()
 
-test_less_row = 25
+test_rows = 25 #用来测试的行数
 
 
 #pd.set_option('display.max_columns', None)
@@ -53,31 +53,36 @@ def Drop_NA(tol_na, dataframe):
 df = Drop_NA(20, df)
 
 df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True )
-print ('2', df.shape)
+print ('drop na后的shape', df.shape)
 df.sort_values( by="code" , ascending=True, inplace=True )
-print ('3', df.shape)
 df.drop_duplicates(['code','time_point'], inplace = True ) #根据code和time_point两个指标删除重复值
-print ('4', df.shape)
+print ('drop duplicates后的shape', df.shape)
 df['yield'] = np.nan
-print ('5', df.shape)
+print ('加了yield一个column后的shape', df.shape)
 ( temp_row, temp_col ) = df.shape
 print (temp_row, temp_col)
-temp_row = test_less_row ##################################################################
+temp_row = test_rows
+
+NA_idex = []  #存在无数据的公司，for循环后删除
 for row in range(0, temp_row):
 	print ('row:',row)
 	temp_stock_int = int(df.iloc[ row, 1 ])
 	temp_stock = "%06d" % temp_stock_int
 	temp_date = df.iloc[ row, -2 ]
+	print("stock", temp_stock)
+	print("start", temp_date)
+	print('end', Closest_TraDt_2(temp_date))
 	temp_df = ts.get_hist_data( temp_stock, start = temp_date, end = Closest_TraDt_2(temp_date) )
-	#print (temp_df)
-	temp_row2 = -1
-	temp_col2 = -1
+	#print ("temp_df", temp_df)
+	if temp_df is None:  #如果没有没有数据，就跳过这家公司，并且存下公司名字，后期删除
+		NA_idex.append(row)
+		continue
 	( temp_row2, temp_col2 ) = temp_df.shape
 	if temp_row2 > 100:
 		yield_yoy = ( temp_df.iloc[ 0, 2 ] - temp_df.iloc[ -1, 2 ] ) / temp_df.iloc[ -1, 2 ]
 		df.iloc[ row, -1 ] = yield_yoy
 
-df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True )
+df.dropna( axis=0, how='any', thresh=None, subset=None, inplace=True ) #删除无数据的公司
 
 def assert_allNumber(dataframe):
 	'''
@@ -159,7 +164,7 @@ def choosing_stock(the_year, the_quarter):
 	print(data_choosing_stock.shape)
 	data_choosing_stock = assert_allNumber(data_choosing_stock)
 	( row_choosing_stock, col_choosing_stock ) = data_choosing_stock.shape
-	row_choosing_stock = test_less_row ######################################################
+	row_choosing_stock = test_rows ######################################################
 	for the_row in range(0, row_choosing_stock):
 
 
@@ -167,7 +172,7 @@ def choosing_stock(the_year, the_quarter):
 			x_temp_choosing_stock.append(data_choosing_stock.iloc[the_row, the_col])
 
 		print('x temp shape 1', len(x_temp_choosing_stock))
-		x_choosing_stock.append(x_temp_choosing_stock)
+		x_choosing_stock.extend(x_temp_choosing_stock)
 
 		print('x shape 2', len(x_choosing_stock))
 
